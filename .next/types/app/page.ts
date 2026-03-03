e6ef59c08fcaf0a082bd814fc192a79bc8ee1d9f
Chintakayala Manish Kumar <ch.manishkumar2003@gmail.com>
2026-03-03 20:35:33 +0530

    Assignment

--- .next/types/app/page.ts
+++ .next/types/app/page.ts
@@ -1,8 +1,8 @@
-// File: C:\Users\chman\Downloads\Assignment-main\Assignment-main\app\page.tsx
-import * as entry from '../../../app/page.js'
+// File: C:\Users\chman\Downloads\Assignment-main\Assignment-main\app\contact\page.tsx
+import * as entry from '../../../../app/contact/page.js'
 import type { ResolvingMetadata } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
-type TEntry = typeof import('../../../app/page.js')
+type TEntry = typeof import('../../../../app/contact/page.js')
 
 // Check that the entry is a valid entry
 checkFields<Diff<{
